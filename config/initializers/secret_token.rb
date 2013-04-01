cd7fa0f1cba7383e669755c3700a02f56f8c354e
bcoe <=>
2013-03-31 22:19:30 -0700

    first commit of moodtube.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moodtube::Application.config.secret_token = 'c1f476e2b98dacd1a14c09431de44bd1ec8286e92573f874323325230ecbf2802244c658db40b3e02a2c6bf8bbcbcf415ccec8d98f8c3888bef14e3451f62b6e'
